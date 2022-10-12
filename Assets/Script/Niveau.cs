7616a881a6e26959222c5dee93c7445c506d829c
jonasamar <91546156+jonasamar@users.noreply.github.com>
2022-10-12 13:55:38 +0200

    Test Change Scene

---
+++ Assets/Script/Niveau.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Niveau : MonoBehaviour
+{
+    public string NomDeScene;
+
+    public void AllerAuNiveau()
+    {
+        SceneManager.LoadScene(NomDeScene);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        AllerAuNiveau();
+    }
+}
